d2539a2d55f723081d0eae3bd62628a750b091b8
Hargobind Atwal <ethaipath@gmail.com>
2019-07-05 20:27:30 -0700

    connecting index.js to api

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+import leaderboard from '../leaderboard.json';
 
 const Index = () => (
   <div>
@@ -8,5 +10,17 @@ const Index = () => (
     <p>Hello Next.js</p>
   </div>
 );
+
+Index.getInitialProps = async function() {
+  console.log(leaderboard);
+  const res = await fetch('https://api.commadotai.com/v2/leaderboard');
+  const leaderboard = await res.json();
+
+  console.log(`Fetched data ${leaderboard}`);
+
+  return {
+    leaderboard
+  };
+};
 
 export default Index;
